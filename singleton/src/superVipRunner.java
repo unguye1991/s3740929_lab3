d09c06779f85c32c0ce52611885d0e53b62c46d8
unguye1991 <84440874+unguye1991@users.noreply.github.com>
2021-09-01 15:58:22 +0700

    Add files via upload

---
+++ singleton/src/superVipRunner.java
@@ -0,0 +1,9 @@
+
+public class superVipRunner {
+	private static void main (String[] args) {
+		superVip mysuperVip = superVip.getInstance();
+		superVip secondsuperVip = superVip.getInstance();
+		superVip thirdsuperVip = superVip.getInstance();
+		
+	}
+}
